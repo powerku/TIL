f7c96525934a04489f2388b48677a1dca911c4f3
구영준 <powerku@naver.com>
2021-03-15 00:01:22 +0900

    소수찾기 commit

---
+++ powerku/소수찾기.js
@@ -0,0 +1,16 @@
+console.log(solution("17"));
+
+/**
+ * 1. 숫자 배열로 만들기
+ * 2. 종이 조각 붙이기
+ * 3. 소수인지 판단하기
+ */
+
+
+function solution(numbers) {
+    var answer = 0;
+
+    var arrNum = numbers.split('');
+
+    return answer;
+}
